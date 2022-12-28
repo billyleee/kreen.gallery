246dec5a2b6af8299c0cc6e16207f1e4a2a82998
Billy lee <32571315+billyleee@users.noreply.github.com>
2022-12-28 15:05:36 +0700

    Update CardDR1.js
    Signed-off-by: Billy lee <32571315+billyleee@users.noreply.github.com>

--- src/components/CardDR1.js
+++ src/components/CardDR1.js
@@ -17,7 +17,7 @@ function CardSkincare() {
           <Card style={{ width: 'auto' }}>
               <Card.Img variant="top" src={gambar1} />
               <Card.Body >
-                  <Card.Title>Black Shine</Card.Title>
+                  <Card.Title>Lip Cream 01</Card.Title>
                   <Card.Text>
                       Some quick example text to build on the card title and make up the
                       bulk of the card's content.
@@ -40,7 +40,7 @@ function CardSkincare() {
               keyboard={false}
               centered>
               <Modal.Header closeButton>
-                  <Modal.Title>Black Shine</Modal.Title>
+                  <Modal.Title>Lip Cream 01</Modal.Title>
               </Modal.Header>
               <Modal.Body>
                   <div class='gambardalam'>
