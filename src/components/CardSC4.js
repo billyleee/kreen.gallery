26a42cb28d3e28e4887220afa4a739e20232f4b1
Billy lee <32571315+billyleee@users.noreply.github.com>
2022-12-29 21:38:36 +0700

    Update CardSC4.js
    Signed-off-by: Billy lee <32571315+billyleee@users.noreply.github.com>

--- src/components/CardSC4.js
+++ src/components/CardSC4.js
@@ -19,9 +19,7 @@ function CardSkincare() {
               <Card.Body >
                   <Card.Title>Probio</Card.Title>
                   <Card.Text>
-//                       Some quick example text to build on the card title and make up the
-//                       bulk of the card's content.
-//                       Some quick example text to build on the card title and make up the
+
 
                   </Card.Text>
                   <Button variant="primary" onClick={handleShow}>
