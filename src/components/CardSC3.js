99f8a7f99eea7faaee62cd87f9748982874016a1
Billy lee <32571315+billyleee@users.noreply.github.com>
2022-12-29 23:27:31 +0700

    Update CardSC3.js
    Signed-off-by: Billy lee <32571315+billyleee@users.noreply.github.com>

--- src/components/CardSC3.js
+++ src/components/CardSC3.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Modal from 'react-bootstrap/Modal';
-import gambar3 from '../asset/Skincare/night-cream-galacto.jpg'
+import gambar3 from '../asset/Decorative/lip-cream-03.jpg'
 
 
 function CardSkincare() {
@@ -10,54 +10,53 @@ function CardSkincare() {
     const handleClose = () => setLgShow(false);
     const handleShow = () => setLgShow(true);
 
-  return (
+    return (
+                <div>
+                            <Card style={{ width: 'auto' }}>
+                                <Card.Img variant="top" src={gambar3} />
+                                <Card.Body >
+                                    <Card.Title>Lip Cream 03</Card.Title>
+                                    <Card.Text>
+                            {/* this is text  */}
+                                    </Card.Text>
+                                    <Button variant="primary" onClick={handleShow}>
+                                        Read More
+                                    </Button>
+                                </Card.Body>
+                            </Card>
 
 
-      <div>
-          <Card style={{ width: 'auto' }}>
-              <Card.Img variant="top" src={gambar3} />
-              <Card.Body >
-                  <Card.Title>Night Cream</Card.Title>
-                  <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                      Some quick example text to build on the card title and make up the
-
-                  </Card.Text>
-                  <Button variant="primary" onClick={handleShow}>
-                      Read More
-                  </Button>
-              </Card.Body>
-          </Card>
-
-
-          <Modal
-              size="lg"
-              show={lgShow}
-              onHide={() => setLgShow(false)}
-              aria-labelledby="example-modal-sizes-title-lg"
-              backdrop="static"
-              keyboard={false}
-              centered>
-              <Modal.Header closeButton>
-                  <Modal.Title>Night Cream</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                  <div class='gambardalam'>
-                      <img class='gambardalam2' src={gambar3} alt="" />
-                  </div>
- <div class='Cardcontainer'><p>
-                Kandungan Galactomyces dapat membantu mencerahkan kulit, membantu mengurangi jumlah sebum pada kulit.
-                  </p></div>
-              </Modal.Body>
-              <Modal.Footer>
-                  <Button class='CloseButtonModal' variant="secondary" onClick={handleClose}>
-                      Close
-                  </Button>
-              </Modal.Footer>
-          </Modal>
-      </div>
-  )
+                            <Modal
+                                size="lg"
+                                show={lgShow}
+                                onHide={() => setLgShow(false)}
+                                aria-labelledby="example-modal-sizes-title-lg"
+                                backdrop="static"
+                                keyboard={false}
+                                centered>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Lip Cream 03</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <div class='gambardalam'>
+                                        <img class='gambardalam2' src={gambar3} alt="" />
+                                    </div>
+                                    <div class='Cardcontainer'>
+                                        <p>
+                               Kandungan Galactomyces dapat membantu mencerahkan kulit, membantu mengurangi jumlah sebum pada kulit.
+                                         </p>
+                                    </div>
+                                </Modal.Body>
+                                <Modal.Footer>
+                                    <Button class='CloseButtonModal' variant="secondary" onClick={handleClose}>
+                                        Close
+                                    </Button>
+                                </Modal.Footer>
+                            </Modal>
+                        </div>
+                        )
 }
 
-export default CardSkincare
+         export default CardSkincare
+
+                  
