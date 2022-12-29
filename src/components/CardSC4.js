2aeb07d8f53ce2d9a9faaba5cd7b0e7d0924b64f
Billy lee <32571315+billyleee@users.noreply.github.com>
2022-12-29 23:29:29 +0700

    Update CardSC4.js
    Signed-off-by: Billy lee <32571315+billyleee@users.noreply.github.com>

--- src/components/CardSC4.js
+++ src/components/CardSC4.js
@@ -10,55 +10,56 @@ function CardSkincare() {
     const handleClose = () => setLgShow(false);
     const handleShow = () => setLgShow(true);
 
-  return (
+    return (
 
 
-      <div>
-          <Card style={{ width: 'auto' }}>
-              <Card.Img variant="top" src={gambar4} />
-              <Card.Body >
-                  <Card.Title>Probio</Card.Title>
-                  <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                      Some quick example text to build on the card title and make up the
+        <div>
+            <Card style={{ width: 'auto' }}>
+                <Card.Img variant="top" src={gambar4} />
+                <Card.Body >
+                    <Card.Title>Probio</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        Some quick example text to build on the card title and make up the
 
-                  </Card.Text>
-                  <Button variant="primary" onClick={handleShow}>
-                      Read More
-                  </Button>
-              </Card.Body>
-          </Card>
+                    </Card.Text>
+                    <Button variant="primary" onClick={handleShow}>
+                        Read More
+                    </Button>
+                </Card.Body>
+            </Card>
 
 
-          <Modal
-              size="lg"
-              show={lgShow}
-              onHide={() => setLgShow(false)}
-              aria-labelledby="example-modal-sizes-title-lg"
-              backdrop="static"
-              keyboard={false}
-              centered>
-              <Modal.Header closeButton>
-                  <Modal.Title>Probio</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                  <div class='gambardalam'>
-                      <img class='gambardalam2' src={gambar4} alt="" />
-                  </div>
-                  <div class='Cardcontainer'><p>
-                  
-                  Probio-C Premium Ampoule Mist diformulasikan sebagai Multi Purpose Skin Care memiliki tekstur yang ringan dan mudah meyerap di kulit wajah dengan sesasi yang menyegarkan. Membantu mengunci kelembaban kulit agar terlihat lebih sehat dan terawat, membantu mengurangi sebum berlebug pada kulit wajah, sekaligus melindungi dari perngaruh buruk sinar ultraviolet. Memiliki kemampuan membantu menjaga skin barrier kulit, membantu mengurangi tanda-tanda penuaan dini, serta meredakan kemerahan pada kulit wajah. Diperkaya dengan Vitamin C dan Niacinamide untuk membantu wajah tampak lebih cerah bercahaya dan sebagai antioksidan. Kulit cerah dengan perawatan yang optimal.
-                  </p></div>
-              </Modal.Body>
-              <Modal.Footer>
-                  <Button class='CloseButtonModal' variant="secondary" onClick={handleClose}>
-                      Close
-                  </Button>
-              </Modal.Footer>
-          </Modal>
-      </div>
-  )
+            <Modal
+                size="lg"
+                show={lgShow}
+                onHide={() => setLgShow(false)}
+                aria-labelledby="example-modal-sizes-title-lg"
+                backdrop="static"
+                keyboard={false}
+                centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Probio</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div class='gambardalam'>
+                        <img class='gambardalam2' src={gambar4} alt="" />
+                    </div>
+                    <div class='Cardcontainer'>
+                        <p>
+                 Probio-C Premium Ampoule Mist diformulasikan sebagai Multi Purpose Skin Care memiliki tekstur yang ringan dan mudah meyerap di kulit wajah dengan sesasi yang menyegarkan. Membantu mengunci kelembaban kulit agar terlihat lebih sehat dan terawat, membantu mengurangi sebum berlebug pada kulit wajah, sekaligus melindungi dari perngaruh buruk sinar ultraviolet. Memiliki kemampuan membantu menjaga skin barrier kulit, membantu mengurangi tanda-tanda penuaan dini, serta meredakan kemerahan pada kulit wajah. Diperkaya dengan Vitamin C dan Niacinamide untuk membantu wajah tampak lebih cerah bercahaya dan sebagai antioksidan. Kulit cerah dengan perawatan yang optimal.
+                 </p>
+                        </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button class='CloseButtonModal' variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    )
 }
 
 export default CardSkincare
