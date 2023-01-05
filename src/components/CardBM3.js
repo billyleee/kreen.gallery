d6ec8c710d4044ad7b56ee2f2e2679f810c61d43
Billy lee <32571315+billyleee@users.noreply.github.com>
2023-01-05 10:27:35 +0700

    Update CardBM3.js
    Signed-off-by: Billy lee <32571315+billyleee@users.noreply.github.com>

--- src/components/CardBM3.js
+++ src/components/CardBM3.js
@@ -19,9 +19,7 @@ function CardSkincare() {
               <Card.Body >
                   <Card.Title>Body Mist Miami Dreams</Card.Title>
                   <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                      Some quick example text to build on the card title and make up the
+                      {/* text is available here */}
 
                   </Card.Text>
                   <Button variant="primary" onClick={handleShow}>
@@ -46,22 +44,11 @@ function CardSkincare() {
                   <div class='gambardalam'>
                       <img class='gambardalam2' src={gambar3} alt="" />
                   </div>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit laboriosam aperiam sequi illo excepturi aliquid doloribus beatae, culpa magnam! Suscipit nihil aliquam facere, aperiam ipsum minus voluptatem iste omnis
-                  bulk of the card's content.
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content. ?
+                  <div class='Cardcontainer'>
+                      <p>
+                          Pewangi badan dengan aromanya yang khas dan segar membantu memberikan wangi yang menyegarkan.
+                  </p>
+              </div>
               </Modal.Body>
               <Modal.Footer>
                   <Button class='CloseButtonModal' variant="secondary" onClick={handleClose}>
