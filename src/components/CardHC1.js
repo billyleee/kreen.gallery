343ca888a849cac15c2a30792df06c91945442d4
Billy lee <32571315+billyleee@users.noreply.github.com>
2023-01-05 10:23:52 +0700

    Update CardHC1.js
    Signed-off-by: Billy lee <32571315+billyleee@users.noreply.github.com>

--- src/components/CardHC1.js
+++ src/components/CardHC1.js
@@ -19,9 +19,7 @@ function CardSkincare() {
               <Card.Body >
                   <Card.Title>Black Shine</Card.Title>
                   <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                      Some quick example text to build on the card title and make up the
+                      {/* text is available here */}
 
                   </Card.Text>
                   <Button variant="primary" onClick={handleShow}>
@@ -46,22 +44,11 @@ function CardSkincare() {
                   <div class='gambardalam'>
                       <img class='gambardalam2' src={gambar1} alt="" />
                   </div>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit laboriosam aperiam sequi illo excepturi aliquid doloribus beatae, culpa magnam! Suscipit nihil aliquam facere, aperiam ipsum minus voluptatem iste omnis
-                  bulk of the card's content.
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content. ?
+                  <div class='Cardcontainer'>
+                      <p>
+                  Black Shine Hair Serum diformulasikan untuk membantu perawatan rambut beruban. Serum yang membantu perawatan rambut beruban. Serum yang membantu melindungi keratin pada folikel rambut, sehingga membantu menjaga kesehatan rambut dari kerusakan dan mempertahankan warna alami pada rambut. Formulanya yang bantu menjaga skin barrier kulit, sehingga membantu menjaga elastisitas kulit kepala, serta membantu melindungi kulit kepala dari kekeringan.
+                  </p>
+              </div>
               </Modal.Body>
               <Modal.Footer>
                   <Button class='CloseButtonModal' variant="secondary" onClick={handleClose}>
